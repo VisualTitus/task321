46cede200e649296fa23ae975311dc136fd5b70e
Jorge Herrera <Ti@MacBook-Air-de-Sabrina-2.local>
2025-05-07 15:55:36 -0400

    🔐 Add login page

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+        <h1>🔐 Login</h1>
+        <p>This will be the login screen for Task321.</p>
+      </div>
+    );
+  }
+  
