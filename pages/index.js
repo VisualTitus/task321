13e983d5b4f708e1cfca648ee0899f6caa36fe06
Jorge Herrera <Ti@MacBook-Air-de-Sabrina-2.local>
2025-05-07 15:46:47 -0400

    ✏️ Replace homepage content

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+        <h1>✅ Welcome to Task321</h1>
+        <p>This is your working homepage.</p>
+      </div>
+    );
+  }
+  
