2015f855c5d484fa665b6450ced470c84745bf1f
Jorge Herrera <Ti@MacBook-Air-de-Sabrina-2.local>
2025-05-08 10:59:00 -0400

    🧱 Fix: replace dashb.tsx with clean valid version

--- pages/dashb.tsx
+++ pages/dashb.tsx
@@ -1,8 +1,12 @@
 
+
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient("https://quqfgjcuxkbgrjaofyec.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1cWZnamN1eGtiZ3JqYW9meWVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MzA2NzEsImV4cCI6MjA2MjIwNjY3MX0.ZOWFNwTc8HUVKcCKg9iIvUzx6KJIWMKC_F5q4uoWVz8");
+
+
+);
 
 type User = {
   email?: string;
